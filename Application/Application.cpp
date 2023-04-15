7ef15433907ba1c09ccb8cc2900fd21568985f15
HumeraTasneemShaik <122573050+HumeraTasneemShaik@users.noreply.github.com>
2023-04-15 01:16:37 -0400

    Update Application.cpp

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,6 +1,6 @@
 // Application.cpp : Defines the exported functions for the DLL.
 //
-
+#include <iostream>
 #include "framework.h"
 #include "Application.h"
 
@@ -13,6 +13,8 @@ int CoolClass::Calc(int a, int b)
 {
     if (a == b)
     {
+        
+        std::cout <<"a is equal to b" << std::endl;
         return 2 * a;
     }
     else
